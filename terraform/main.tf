37310eb80b90a7c316c6ef173f504965e119fd52
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-21 05:30:28 +0100

    terraform init

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  cloud {
+    organization = "tiger_projects"
+    workspaces {
+      name = "streaming-kafka-kinesis"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
