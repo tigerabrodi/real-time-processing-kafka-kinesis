2a1bb90bfa34230bfe7b07cd6ec28e6eaecd4eda
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-22 18:01:21 +0100

    cleanup, output shouldn't been removed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,3 +4,9 @@ output "scram_secret_name" {
   value     = module.aws_msk.scram_secret_name
   sensitive = true
 }
+
+
+output "kafka_cluster_arn" {
+  value     = module.aws_msk.kafka_cluster_arn
+  sensitive = true
+}
